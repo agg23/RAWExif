29c53ef5cca77d26960e720021cfbbac58388206
Adam Gastineau <adam@appcannonsoftware.com>
2021-09-25 12:43:09 -0700

    Initial Commit

---
+++ RAWExif/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RAWExif
+//
+//  Created by Adam Gastineau on 9/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RAWExif/RAWExifApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAWExifApp.swift
+//  RAWExif
+//
+//  Created by Adam Gastineau on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct RAWExifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
